9120e2f0367628dd493df4d7602f5a68bc85dcb6
xwzhou1314 <xwzhou1314@gmail.com>
2019-10-30 18:52:27 +0800

    初始化

--- hs-common/src/main/java/com/hsource/common/enums/ExceptionEnum.java
+++ hs-common/src/main/java/com/hsource/common/enums/ExceptionEnum.java
@@ -13,7 +13,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ExceptionEnum {
-    PRICE_CANNOT_BE_NULL(400,"价格部位空")
+    PRICE_CANNOT_BE_NULL(400,"价格部位空"),
+    BRAND_SAVE_FAILED(500, "新增品牌失败"),
+    UPLOAD_FILE_ERROR(500, "上传文件失败"),
+    INVALID_TYPE_ERROR(400, "文件类型不匹配")
     ;
     private int code;
     private String msg;
