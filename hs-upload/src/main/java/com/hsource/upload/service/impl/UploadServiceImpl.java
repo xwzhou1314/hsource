85e7b456c7ed500f049711b62c174a43609ddb9d
xwzhou1314 <xwzhou1314@gmail.com>
2019-11-18 21:39:23 +0800

    完善上穿图片微服务

--- hs-upload/src/main/java/com/hsource/upload/service/impl/UploadServiceImpl.java
+++ hs-upload/src/main/java/com/hsource/upload/service/impl/UploadServiceImpl.java
@@ -1,50 +1,62 @@
 package com.hsource.upload.service.impl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.hsource.common.enums.ExceptionEnum;
 import com.hsource.common.exception.HsException;
+import com.hsource.upload.config.UploadProperties;
 import com.hsource.upload.service.UploadService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 @Slf4j
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadServiceImpl implements UploadService {
 
-    private static  final List<String> ALLOW_TYPES = Arrays.asList("image/jpeg", "image/png", "image/bmp");
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private UploadProperties uploadProperties;
 
     @Override
     public String uploadImage(MultipartFile file) {
         try {
             // 检验文件类型
             String contenType = file.getContentType();
-            if(! ALLOW_TYPES.contains(contenType)){
+            if (!uploadProperties.getAllowTypes().contains(contenType)) {
                 throw new HsException(ExceptionEnum.INVALID_TYPE_ERROR);
             }
             // 检验文件内容
             BufferedImage image = ImageIO.read(file.getInputStream());
-            if(null == image){
+            if (null == image) {
                 throw new HsException(ExceptionEnum.INVALID_TYPE_ERROR);
             }
-            // 1 保存图片
-            // 1.1 生成保存目录
-            // File dir = new File("E:\\upload");
-            File dir = new File("/home/upload/", file.getOriginalFilename());
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-            // 1.2 保存图片
-            file.transferTo(new File(dir, file.getOriginalFilename()));
+
+            // 1 保存图片到FastDFS
+            String extensionName = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), extensionName, null);
+
+            // 1 保存图片到本地
+//            // 1.1 生成保存目录
+//            File dir = new File("F:\\upload", file.getOriginalFilename());
+//            if (!dir.exists()) {
+//                dir.mkdirs();
+//            }
+//            // 1.2 保存图片
+//            file.transferTo(new File(dir, file.getOriginalFilename()));
 
             // 2.返回路径
-            return "http://image.hsource.com/" + file.getOriginalFilename();
+            return uploadProperties.getBaseUrl() + storePath.getFullPath();
         } catch (IOException e) {
             // 上传失败
             log.error("上传失败", e);
--- hs-upload/src/main/java/com/hsource/upload/web/UploadController.java
+++ hs-upload/src/main/java/com/hsource/upload/web/UploadController.java
@@ -3,15 +3,12 @@ package com.hsource.upload.web;
 
 import com.hsource.upload.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("upload")
@@ -21,7 +18,7 @@ public class UploadController {
     private UploadService uploadService;
 
     @PostMapping("image")
-    public ResponseEntity<String> uploadImage(@RequestParam("file")MultipartFile file) {
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
         String url = uploadService.uploadImage(file);
         return ResponseEntity.ok(url);
     }
